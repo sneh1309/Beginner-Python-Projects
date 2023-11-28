556f77cea907b73faf4e45a0352d03b4c4d4a54b
sneh patel <73465943+sneh1309@users.noreply.github.com>
2023-11-28 14:55:00 +0000

    Update main.py

--- Jarvis_AI_Bot/main.py
+++ Jarvis_AI_Bot/main.py
@@ -9,7 +9,7 @@ import smtplib
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[1].id)
 
 
 def speak(audio):
